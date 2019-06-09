8f82c833d25050e144c65eb0521920858d784302
Tanusha Jayasinghe <tanushajayasinghe@gmail.com>
2019-06-09 22:33:00 +0530

    Rating Dialog added

--- app/src/main/java/com/tanushaj/element/LevelActivity.java
+++ app/src/main/java/com/tanushaj/element/LevelActivity.java
@@ -1,5 +1,6 @@
 package com.tanushaj.element;
 
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -16,26 +17,33 @@ import com.arges.sepan.argmusicplayer.PlayerViews.ArgPlayerSmallView;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class LevelActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnSeekCompleteListener, MediaPlayer.OnPreparedListener {
+import angtrim.com.fivestarslibrary.FiveStarsDialog;
+import angtrim.com.fivestarslibrary.NegativeReviewListener;
+import angtrim.com.fivestarslibrary.ReviewListener;
+
+public class LevelActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnSeekCompleteListener, MediaPlayer.OnPreparedListener, NegativeReviewListener, ReviewListener {
 
     MediaPlayer mp;
     Button playPauseBtn;
+    Button cancelSessionBtn;
     private int playBackPosition = 0;
     TextView playDurationTxt;
     TextView totalDurationTimeTxt;
-        Handler handler;
+//    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_level);
         mp = new MediaPlayer();
-        handler = new Handler();
+//        handler = new Handler();
 
         playPauseBtn = findViewById(R.id.playOrPauseBtn);
+        cancelSessionBtn = findViewById(R.id.cancel_session);
         playDurationTxt = findViewById(R.id.playDurationTxt);
         totalDurationTimeTxt = findViewById(R.id.totalDurationTimeTxt);
         playPauseBtn.setOnClickListener(this);
+        cancelSessionBtn.setOnClickListener(this);
 //        smallView
     }
 
@@ -45,7 +53,22 @@ public class LevelActivity extends AppCompatActivity implements View.OnClickList
             case R.id.playOrPauseBtn:
                 playOrPause();
                 break;
+            case R.id.cancel_session:
+                endSession();
+                break;
         }
+    }
+
+    private void endSession() {
+        FiveStarsDialog fiveStarsDialog = new FiveStarsDialog(this,"tanushajayasinghe@gmail.com");
+        fiveStarsDialog.setRateText("Your custom text")
+                .setTitle("Your custom title")
+                .setForceMode(false)
+//                .setStarColor(Color.YELLOW)
+                .setUpperBound(2) // Market opened if a rating >= 2 is selected
+                .setNegativeReviewListener(this) // OVERRIDE mail intent for negative review
+                .setReviewListener(this) // Used to listen for reviews (if you want to track them )
+                .showAfter(0);
     }
 
     private void playOrPause() {
@@ -106,15 +129,35 @@ public class LevelActivity extends AppCompatActivity implements View.OnClickList
     public void onPrepared(final MediaPlayer mp) {
         mp.start();
         totalDurationTimeTxt.setText(String.format("%s Min", String.valueOf(TimeUnit.MILLISECONDS.toMinutes(mp.getDuration()))));
-        Runnable notification = new Runnable() {
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
 
+            @Override
             public void run() {
-                playDurationTxt.setText(String.format("%s Sec", String.valueOf(TimeUnit.MILLISECONDS.toSeconds(mp.getCurrentPosition()))));
-
+                try{
+                    playDurationTxt.setText(String.format("%s Sec", String.valueOf(TimeUnit.MILLISECONDS.toSeconds(mp.getCurrentPosition()))));
+                }
+                catch (Exception e) {
+                    // TODO: handle exception
+                }
+                finally{
+                    //also call the same runnable to call it at regular interval
+                    handler.postDelayed(this, 300);
+                }
             }
-
         };
 
-        handler.postDelayed(notification, 300);
+        handler.post(runnable);
+
+    }
+
+    @Override
+    public void onNegativeReview(int i) {
+
+    }
+
+    @Override
+    public void onReview(int i) {
+
     }
 }
